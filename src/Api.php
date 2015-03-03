1b7ccf619101471422a4497bc9cd990d21dfb5d2
Vincent Boiardt <vincent@boiardt.se>
2015-03-03 10:38:58 +0100

    Renamed files

--- src/Api.php
+++ src/Api.php
@@ -4,7 +4,7 @@ namespace OWC\Silk;
 use Monolog\Logger,
 	Monolog\Handler\StreamHandler;
 
-class Silk {
+class Api {
 
 	/*
 	|-----------------------------------------------------------
@@ -29,14 +29,14 @@ class Silk {
 
 	public function __construct( $props ) {
 		foreach ( $props as $key => $value ) {
-			if ( isset( Silk::${$key} ) ) {
-				Silk::${$key} = $value;
+			if ( isset( Api::${$key} ) ) {
+				Api::${$key} = $value;
 			}
 		}
 
 		// create a log channel
-		Silk::$log = new Logger( 'silk' );
-		Silk::$log->pushHandler( new StreamHandler( dirname( __FILE__ ) . '/../silk.log', Logger::DEBUG ) );
+		Api::$log = new Logger( 'silk' );
+		Api::$log->pushHandler( new StreamHandler( dirname( __FILE__ ) . '/../silk.log', Logger::DEBUG ) );
 	}
 
 	/*
@@ -46,7 +46,7 @@ class Silk {
 	*/
 
 	public static function get_url( $append = '/' ) {
-		return Silk::$url . $append;
+		return Api::$url . $append;
 	}
 
 	/*
@@ -56,14 +56,14 @@ class Silk {
 	*/
 
 	// generic
-	public static function post( $url, $data = array() )   { return Silk::call_api( 'POST', $url, $data ); }
-	public static function put( $url, $data = array() )    { return Silk::call_api( 'PUT', $url, $data ); }
-	public static function get( $url, $data = array() )    { return Silk::call_api( 'GET', $url, $data ); }
-	public static function delete( $url, $data = array() ) { return Silk::call_api( 'DELETE', $url, $data ); }
+	public static function post( $url, $data = array() )   { return Api::call_api( 'POST', $url, $data ); }
+	public static function put( $url, $data = array() )    { return Api::call_api( 'PUT', $url, $data ); }
+	public static function get( $url, $data = array() )    { return Api::call_api( 'GET', $url, $data ); }
+	public static function delete( $url, $data = array() ) { return Api::call_api( 'DELETE', $url, $data ); }
 
 	// call the Shopify api
 	public static function call_api( $method, $url, $raw_data = array() ) {
-		$url = Silk::get_url( $url );
+		$url = Api::get_url( $url );
 
 		$data = http_build_query( $raw_data );
 		 
@@ -83,18 +83,18 @@ class Silk {
 			
 			case 'POST':
 				curl_setopt( $ch, CURLOPT_POST, true );
-				curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
+				curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $raw_data ) );
 				break;
 		}
 
-		curl_setopt( $ch, CURLOPT_HTTPHEADER, array( 'Accept: application/json', 'API-Authorization: ' . Silk::$secret ) );
+		curl_setopt( $ch, CURLOPT_HTTPHEADER, array( 'Accept: application/json', 'API-Authorization: ' . Api::$secret ) );
 		curl_setopt( $ch, CURLOPT_URL, $url );
 		 
 		$response = curl_exec($ch);
 
 		curl_close($ch);
 
-		Silk::$log->addInfo(
+		Api::$log->addInfo(
 			'Silk',
 			compact( 'url', 'method', 'data', 'response' )
 		);
