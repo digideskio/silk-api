d12d7c0c2e2777f02b39d4cc30c853bc1ca92e28
Vincent Boiardt <vincent@boiardt.se>
2015-06-12 18:09:06 +0200

    SSL fix

--- src/Api.php
+++ src/Api.php
@@ -89,6 +89,7 @@ class Api {
 
 		curl_setopt( $ch, CURLOPT_HTTPHEADER, array( 'Accept: application/json', 'API-Authorization: ' . Api::$secret ) );
 		curl_setopt( $ch, CURLOPT_URL, $url );
+		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 ); 
 		 
 		$response = curl_exec($ch);
 
